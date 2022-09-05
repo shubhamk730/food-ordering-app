c8bec72d33400897c7bfb06dd6e20a381f368ee7
shubhamk730 <skaushik241000@gmail.com>
2022-09-05 10:53:15 +0530

    Token verification mechanism updated

--- backend/middleware/verifyToken.js
+++ backend/middleware/verifyToken.js
@@ -25,4 +25,28 @@ const verifyTokenAdmin = (req, res, next) => {
   }
 };
 
-module.exports = verifyTokenAdmin;
+const verifyTokenUser = (req, res, next) => {
+  if (
+    req.headers.authorization &&
+    req.headers.authorization.startsWith("Bearer")
+  ) {
+    try {
+      const token = req.headers.authorization.split(" ")[1];
+
+      const decoded = jwt.verify(token, process.env.JWT_SECRET_USER);
+
+      const user = Admin.findById(decoded.id).select("-password");
+      req.user = user;
+      next();
+    } catch (err) {
+      res.status(401);
+      throw new Error("Not authorized, token failed");
+    }
+  } else {
+    const error = new Error("Not authorized");
+    error.statusCode = 401;
+    throw error;
+  }
+};
+
+module.exports = { verifyTokenAdmin, verifyTokenUser };
